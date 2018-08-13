95a3b88a2f39c800b6562b891ff5fd2352f1472c
kambae <ryanwp526@gmail.com>
2018-08-13 17:15:39 +1000

    tutorial solar system

--- root/Assets/SolarSystem/_Scripts/SolarSystemSpawner.gd
+++ root/Assets/SolarSystem/_Scripts/SolarSystemSpawner.gd
@@ -44,6 +44,14 @@ func _ready():
 #	planet_names_file.open(planet_names_path, File.READ)
 #	for line in planet_names_file.get_as_text().split("\n"):
 #		planet_names.append(line)
+	var solar_system = solar_system_scene.instance()
+	add_child(solar_system)
+	solar_system.planet_enemies_mean = 1
+	solar_system.planet_enemies_randomness = 0
+	solar_system.sun_enemies_mean = 2
+	solar_system.sun_enemies_randomness = 0
+	solar_system.init(1000, 2500,sun_sprite_array,planet_sprite_array, "Tutorial", planet_names)
+	solar_system.position = Vector2(600, 600)
 	start_spawn()
 
 func _process(delta):
@@ -75,7 +83,6 @@ func build_animations(path, i):
 func create_solar_system(x, y, n, spread):
 	var solar_system = solar_system_scene.instance()
 	add_child(solar_system)
-	print(sun_sprite_array)
 	solar_system.init(n, spread, sun_sprite_array,planet_sprite_array, sun_names[randi()%len(sun_names)], planet_names)
 	solar_system.position.x = x
 	solar_system.position.y = y
@@ -94,11 +101,11 @@ func spawn(n, pos, origin, dist):
 			sun_grid[[goal_x_grid, goal_y_grid]] = true
 	
 func start_spawn():
-	spawn(1, Vector2(1, 0), Vector2(0, 0), 4000)
-	spawn(1, Vector2(-1, 0), Vector2(0, 0),5000)
-	spawn(1, Vector2(0, 1), Vector2(0, 0), 5000)
-	spawn(1, Vector2(0,-1), Vector2(0, 0), 5000)
-	spawn(1, Vector2(1, 0), Vector2(0, 0), 8000)
-	spawn(1, Vector2(-1, 0), Vector2(0, 0),8000)
-	spawn(2, Vector2(0, 1), Vector2(0, 0), 8000)
-	spawn(2, Vector2(0,-1), Vector2(0, 0), 8000)
+	spawn(1, Vector2(1, 0), Vector2(0, 0), 7000)
+	spawn(1, Vector2(-1, 0), Vector2(0, 0),7000)
+	spawn(1, Vector2(0, 1), Vector2(0, 0), 7000)
+	spawn(1, Vector2(0,-1), Vector2(0, 0), 7000)
+	spawn(1, Vector2(1, 0), Vector2(0, 0), 7000)
+	spawn(1, Vector2(-1, 0), Vector2(0, 0),7000)
+	spawn(1, Vector2(0, 1), Vector2(0, 0), 7000)
+	spawn(1, Vector2(0,-1), Vector2(0, 0), 7000)
