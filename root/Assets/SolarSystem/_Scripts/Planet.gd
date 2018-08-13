b0801e02e72ae7572c161f4795de2a0a91cc1e54
kambae <ryanwp526@gmail.com>
2018-08-13 12:57:21 +1000

    sun names from array and more exports for readable and changable code also reduced enemy spawns

--- root/Assets/SolarSystem/_Scripts/Planet.gd
+++ root/Assets/SolarSystem/_Scripts/Planet.gd
@@ -13,9 +13,3 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
-
-func _on_EnemySpawnArea_body_entered(body):
-	print("don")
-	if(body.get_name == "Player"):
-		_spawn_enemies()
